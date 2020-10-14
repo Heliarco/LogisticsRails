d8bf9459ab72f11f4c246d39176b426f01653662
Alexander Brandbyge <alexander@brandbyge.dk>
2020-10-14 15:02:24 +0200

    RUby block now drops it self on mine

--- src/main/java/dk/vertexspace/blocks/RubyBlock.java
+++ src/main/java/dk/vertexspace/blocks/RubyBlock.java
@@ -11,6 +11,7 @@ public class RubyBlock extends Block {
         super(Block.Properties.create(Material.IRON)
                 .hardnessAndResistance(5.0f, 6.0f)
                 .sound(SoundType.METAL)
+                .func_235861_h_() // Mapping missing of "setrequirestool"
                 .harvestLevel(2) // 0 is wood, 1 is stone, 2 is iron, 3 is diamond etc.
                 .harvestTool(ToolType.PICKAXE));
     }
