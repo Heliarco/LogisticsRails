97e497c05b49b9177ebe116550c9999c34fbe717
Alexander Brandbyge <alexander@brandbyge.dk>
2020-11-20 01:41:33 +0100

    More block model fun, added first draft of multi export driver script, so far it can just sequentially open the models

---
+++ blockbenchmodels/blocks/autoexport.ps1
@@ -0,0 +1,36 @@
+# This script relies on a HEAVY set of assumptions. 
+# 1. That blockbench is tied to the bbmodel autolaunch config
+# 2. that the multiexport is installed correctly,
+# 3. that the running machine has the horsepower to load blockbench models quickly enough to not still be doing loading when the export process triggers.
+# 4. Luck
+# ANYWAY, here is what it does
+# Remove old models
+# Remove old textures
+# Foreach bbmodel file, open it with an env arg that is caught from the multiexport plugin, telling it to wait a bit then autostart the exporting procedure
+# Copy over all textures
+
+# If it works, its one-click rebuild all models... 
+
+Set-Location $PSScriptRoot
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+Get-ChildItem *bbmodel | foreach { 
+
+    Start-Process $_
+    # wait for BB to exit
+    while($true){
+        Start-Sleep -s 1
+        
+     
+        $x = Get-Process BlockBench* -ErrorAction SilentlyContinue
+        if (!$x){
+            break;
+        }
+    }
+    
+
+
+    #    Remove-Item -Path $_.FullName 
+
+}
