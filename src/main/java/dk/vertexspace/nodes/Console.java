797d776335f3fd50f571c91e7bd9e951cce2d0bf
Alexander Brandbyge <alexander@brandbyge.dk>
2020-12-09 13:07:54 +0100

    Console code added

--- src/main/java/dk/vertexspace/nodes/Console.java
+++ src/main/java/dk/vertexspace/nodes/Console.java
@@ -1,12 +1,21 @@
 package dk.vertexspace.nodes;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ToolType;
 
-public class Console extends Block {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class Console extends DirectionalBlock {
+
     public Console() {
         super(AbstractBlock.Properties.create(Material.MISCELLANEOUS)
                 .hardnessAndResistance(1.0f, 1.0f)
@@ -16,6 +25,38 @@ public class Console extends Block {
                 .harvestTool(ToolType.PICKAXE)
         );
     }
+    @Nullable
+    @Override
+    // Called when block is placed.
+    // Here we can calculate the state of the block before ultimate placement
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isTransparent(BlockState state) {
+        return false;
+    }
 
 
+    @Override
+    @SuppressWarnings("deprecation")
+    @Nonnull
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+    }
+
+    @Override
+    public void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+    {
+        builder.add(FACING);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    @Nonnull
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(FACING, rot.rotate(state.get(FACING)));
+    }
 }
