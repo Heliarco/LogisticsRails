279c557266bd13e7b85ecb5dbb26766c6c357237
Alexander Brandbyge <alexander@brandbyge.dk>
2020-12-30 16:26:24 +0100

    Railometer now has a mode change

--- src/main/java/dk/vertexspace/items/Railometer.java
+++ src/main/java/dk/vertexspace/items/Railometer.java
@@ -2,51 +2,133 @@ package dk.vertexspace.items;
 
 import dk.vertexspace.constants.TabGroups;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.command.arguments.NBTCompoundTagArgument;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.*;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
 import java.util.HashSet;
+import java.util.List;
 
 public class Railometer extends ToolItem {
     public Railometer() {
         super(-2f, -1, ItemTier.IRON, new HashSet<Block>(),
                 new Item.Properties().group(TabGroups.TAB));
     }
+    private static final String MODE_TAG = "mode";
 
-//
-//
-//    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-//        ItemStack itemstack = playerIn.getHeldItem(handIn);
-//        if (playerIn.fishingBobber != null) {
-//            if (!worldIn.isRemote) {
-//                int i = playerIn.fishingBobber.handleHookRetraction(itemstack);
-//                itemstack.damageItem(i, playerIn, (player) -> {
-//                    player.sendBreakAnimation(handIn);
-//                });
-//            }
-//
-//            worldIn.playSound((PlayerEntity)null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.NEUTRAL, 1.0F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
-//        } else {
-//            worldIn.playSound((PlayerEntity)null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
-//            if (!worldIn.isRemote) {
-//                int k = EnchantmentHelper.getFishingSpeedBonus(itemstack);
-//                int j = EnchantmentHelper.getFishingLuckBonus(itemstack);
-//                worldIn.addEntity(new FishingBobberEntity(playerIn, worldIn, j, k));
-//            }
-//
-//            playerIn.addStat(Stats.ITEM_USED.get(this));
-//        }
-//
-//        return ActionResult.func_233538_a_(itemstack, worldIn.isRemote());
-//    }
+    private enum RailometerMode {
+        CONNECTION_LIST(0, "List connections"),
+        CONTROLLER_SCAN(1, "Scan for controller");
+
+
+        private final int value;
+        private final String description;
+        RailometerMode(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+        public String getDescription(){
+            return this.description;
+        }
+        public int getValue() {
+            return this.value;
+        }
+        public static RailometerMode fromValue(int value)
+        {
+            RailometerMode[] As = RailometerMode.values();
+            for(int i = 0; i < As.length; i++)
+            {
+                if(As[i].getValue() == value)
+                    return As[i];
+            }
+            return CONNECTION_LIST;
+        }
+
+
+    }
+
+    // Right click on block
+    @Override
+    public ActionResultType onItemUse(ItemUseContext context) {
+
+
+        World worldIn = context.getWorld();
+        // We really only care about the logical client thread here. We just want to display data to the user
+        if (worldIn.isRemote()) {
+            return ActionResultType.func_233537_a_(worldIn.isRemote);
+        }
+
+        BlockPos blockpos = context.getPos();
+        BlockState blockstate = worldIn.getBlockState(blockpos);
+
+        RailometerMode mode = getModeFromItemStack(context.getItem());
+        switch (mode) {
+            case CONNECTION_LIST:
+                break;
+            default:
+
+                break;
+        }
+
+
+
+
+        return ActionResultType.func_233537_a_(worldIn.isRemote);
+
+
+    }
+
+    private RailometerMode getModeFromItemStack(ItemStack stack){
+        CompoundNBT tag = stack.getOrCreateTag();
+        if(tag.contains(MODE_TAG)){
+            return RailometerMode.fromValue(tag.getInt(MODE_TAG));
+        }
+        else {
+            return RailometerMode.CONNECTION_LIST;
+        }
+    }
+
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new StringTextComponent("Mode - " + RailometerMode.fromValue(stack.getOrCreateTag().getInt(MODE_TAG)).getDescription()));
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+    }
+
+    // Click in "air"
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+
+        if (playerIn.isSneaking()){ // Shift right-click to change mode
+            CompoundNBT tag = itemstack.getOrCreateTag();
+            RailometerMode mode;
+            if (tag.contains(MODE_TAG)){
+                mode = RailometerMode.fromValue(tag.getInt(MODE_TAG));
+            }
+            else {
+                mode = RailometerMode.CONNECTION_LIST;
+            }
+            mode = RailometerMode.fromValue(mode.getValue()+1);
+            tag.putInt(MODE_TAG, mode.getValue());
+            itemstack.setTag(tag);
+        }
+
+
+        return ActionResult.func_233538_a_(itemstack, worldIn.isRemote());
+    }
 
 
 }
